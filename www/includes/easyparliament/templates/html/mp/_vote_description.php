f09b8ed7619691fdfa118cc7b679fa772cceb486
Zarino Zappia <mail@zarino.co.uk>
2017-01-27 12:24:04 +0000

    Styling for vote description evidence string

--- www/includes/easyparliament/templates/html/mp/_vote_description.php
+++ www/includes/easyparliament/templates/html/mp/_vote_description.php
@@ -3,7 +3,7 @@
     <?php if ( $show_link ) { ?>
         <a class="vote-description__source" href="<?= $link ?>">Show votes</a>
         <?php if (isset($key_vote)) { ?>
-        <br><small><?= $key_vote['summary'] ?></small>
+        <small class="vote-description__evidence"><?= $key_vote['summary'] ?></small>
         <?php } ?>
     <?php } ?>
 </li>
